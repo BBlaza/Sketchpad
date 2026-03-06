6edaeacd8b436d2d395f45a866a0248b11d1c679
Brazza <jjia@ufl.edu>
2026-03-05 21:54:45 -0500

    Set up brush
    All features of brushes are finished

--- tools/brush/brush.gd
+++ tools/brush/brush.gd
@@ -3,23 +3,86 @@ extends Tool
 
 @export var title: String = "Brush"
 @export var stamp: Texture2D = PlaceholderTexture2D.new()
-@export var width: float = 1.0
+@export var width: float
+@export var hardness: float
 
-var _line: Line2D
 var color: Color
+var _stroke_node: Node2D
+var _stamp_tex: Texture2D
+var _last_pos: Vector2
+var _has_last = false
 
 func _init() -> void:
 	name = "Brush"
 
+
 func on_pointer_down(_position: Vector2, _canvas: Canvas) -> void:
-	_line = Line2D.new()
-	_line.width = width
-	_line.default_color = color
-	_canvas.dynamic_node.add_child(_line)
+	_stroke_node = Node2D.new()
+	_canvas.dynamic_node.add_child(_stroke_node)
+	_stamp_tex = generate_stamp(hardness)
+	_has_last = true
+	_last_pos = _position
+	_place_stamp(_last_pos)
+
 
 func on_pointer_move(_position: Vector2, _canvas: Canvas) -> void:
-	if _line and _canvas._project:
-		_line.add_point(_position)
+	if not _stroke_node or not _canvas._project or not _has_last :
+		return
+	
+	var delta := _last_pos - _position
+	var dist := delta.length()
+	var dir := delta / dist
+
+	var t := 0.0
+	while t <= dist:
+		if (dir * t).length() > (width / _stamp_tex.get_width() * 10):
+			_place_stamp(_last_pos + dir * t)
+			_last_pos = _position
+		t += (width / _stamp_tex.get_width() * 10)
+
 
 func on_pointer_up(_position: Vector2, _canvas: Canvas) -> void:
+	_has_last = false
 	_canvas.bake_page()
+
+
+func generate_stamp(hardness: float) -> Texture2D:
+	var size_px = stamp.get_height() * 10
+	var img = stamp.get_image()
+	var org_img = img
+	img.resize(size_px, size_px)
+	var center = Vector2(size_px * 0.5, size_px * 0.5)
+	var radius = size_px * 0.5 * sqrt(2)
+	var feather = 1.0 - hardness
+	var inner = radius * (1 - feather)
+	
+	for y in size_px:
+		for x in size_px:
+			var distance = center.distance_to(Vector2(x + 0.5, y + 0.5))
+
+			var transparency = 0.0
+			if distance <= inner:
+				transparency = 1.0
+			elif distance < radius:
+				transparency = pow(1.0 - (distance - inner) / (radius - inner), 3)
+			else:
+				transparency = 0
+			
+			img.set_pixel(x, y, Color(org_img.get_pixel(x, y), org_img.get_pixel(x, y).a * transparency))
+			
+	#img.unlock()
+	img = ImageTexture.create_from_image(img)
+	return img
+
+
+func _place_stamp(_position: Vector2) -> void:
+	var s = Sprite2D.new()
+	s.texture = _stamp_tex
+	s.position = _position
+	s.modulate = color
+
+	var tex_w = float(_stamp_tex.get_width())
+	var scale_factor = width / tex_w
+	s.scale = Vector2.ONE * scale_factor
+
+	_stroke_node.add_child(s)
--- views/editor/containers/edit_extras/brush_handler.gd
+++ views/editor/containers/edit_extras/brush_handler.gd
@@ -13,21 +13,27 @@ var brushes =[
 ]
 
 var brush_width = 2.5
+var brush_hardness = 1.0
 var brush_color = Color.BLACK
 
 func _ready() -> void:	
-	
 	brush_list.item_selected.connect(_on_brush_selected)
 	thick_sldr.value_changed.connect(_on_thickness_changed)
+	hard_sldr.value_changed.connect(_on_hardness_changed)
 	color_picker.color_changed.connect(_on_color_changed)
 
 	brush_list.select(0)
 	_on_brush_selected(0)
 	thick_sldr.value = brush_width
+	hard_sldr.value = brush_hardness
 
 func _on_thickness_changed(value: float) -> void:
-	brush_width = thick_sldr.value
+	brush_width = value
 	root.current_tool.width = brush_width
+	
+func _on_hardness_changed(value: float) -> void:
+	brush_hardness = value
+	root.current_tool.hardness = brush_hardness
 
 func _on_color_changed(color: Color) -> void:
 	brush_color = color
@@ -35,6 +41,7 @@ func _on_color_changed(color: Color) -> void:
 
 func _on_brush_selected(index: int) -> void:
 	root.current_tool = brushes[index]
+	root.current_tool.hardness = brush_hardness
 	root.current_tool.width = brush_width
 	root.current_tool.color = brush_color
 	
